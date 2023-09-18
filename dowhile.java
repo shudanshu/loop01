c272cf316f468da408e26a4f43138a8d4afa5a08
sudhanshu kumar <145319097+shudanshu@users.noreply.github.com>
2023-09-18 11:44:56 +0530

    Add files via upload

---
+++ dowhile.java
@@ -0,0 +1,12 @@
+public class dowhile {
+    public static void main(String[] args) {
+        int num = 1;
+        do{
+            System.out.println("hello sudhanshu");
+            num++;
+        } 
+        while(num<=10);
+
+
+    }
+}
---
+++ loop.java
@@ -0,0 +1,11 @@
+public class loop {
+    public static void main(String[] args) {
+        int counter = 200; 
+        while(counter>100) {
+            System.out.println("hello sudhanshu");
+            counter++;
+        }
+    
+    }
+
+}
